d23df899f7aa972f54b9940374d5fb054a78ff9c
Kribas Rimal <carlousrimal@gmail.com>
2021-07-05 18:50:56 +0545

    made changes

---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    token: 'BQD89eMgRt5iEPrPrmrrQVB5Ga3tZ0bDJLx8kriwb5Sz6rVIfoSg342o1n_xgaS21NIgcsfGRN9HuJJPMrBqgkW_6vpJR88iPXByJOcoBjR8UG-gfJdmb9wLtLAPQh9d332MGYdhE5XujBlxCN-oZcaZuL_fW5u1jFQZGwRzGwN07D_i'
+    
+}
+
+const reducer = (state, action) => {
+    console.log(action);
+
+    switch(action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
+
+        default:
+            return state;
+    }
+};
+
+export default reducer
